7d13e06a18702bb04712ccf2ccd6e0599768a632
ssan93 <san.steven97@gmail.com>
2022-09-21 11:11:30 +0200

    fix(hosting): adapt config for next

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
-module.exports = {
-  images: {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {    
+    loader: 'akamai',
+    path: '',
     domains: ["github.com", "i.imgur.com", "res.cloudinary.com"],
-  },
-};
+  }
+}
+
+module.exports = nextConfig
