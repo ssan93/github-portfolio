347afcc017efba463c2aa9634bf8d33bcf047af2
ssan93 <san.steven97@gmail.com>
2022-09-26 11:56:54 +0200

    fix(footer): Change a tag to p tag with hover

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,20 +4,20 @@ const Footer = () => {
   return (
     <div className="flex flex-wrap items-center space-x-1 space-y-2 justify-evenly text-xs text-[#59A6FF] py-8">
       <p className="text-gray-500 text-xs">© 2022 GitHub, Inc.</p>
-      <a href="#">Terms</a>
-      <a href="#">Privacy</a>
-      <a href="#">Security</a>
-      <a href="#">Status</a>
-      <a href="#">Docs</a>
+      <p className="cursor-pointer">Terms</p>
+      <p className="cursor-pointer">Privacy</p>
+      <p className="cursor-pointer">Security</p>
+      <p className="cursor-pointer">Status</p>
+      <p className="cursor-pointer">Docs</p>
 
       <AiFillGithub className="text-2xl text-gray-600 hidden md:block" />
 
-      <a href="#">Contact Github</a>
-      <a href="#">Pricing</a>
-      <a href="#">API</a>
-      <a href="#">Training</a>
-      <a href="#">Blog</a>
-      <a href="#">About</a>
+      <p className="cursor-pointer">Contact Github</p>
+      <p className="cursor-pointer">Pricing</p>
+      <p className="cursor-pointer">API</p>
+      <p className="cursor-pointer">Training</p>
+      <p className="cursor-pointer">Blog</p>
+      <p className="cursor-pointer">About</p>
     </div>
   );
 };
