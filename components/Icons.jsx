52340f85b6cd232de1957ca8b4cbd00c2ce6f363
ssan93 <san.steven97@gmail.com>
2022-10-07 23:43:11 +0200

    feat(icons): add link to refs

--- components/Icons.jsx
+++ components/Icons.jsx
@@ -40,67 +40,67 @@ const Icons = ({ tag }) => {
 const renderSwitch = (tag) => {
   switch(tag){
     case "react":
-      return <DiReact className="text-blue-400 text-lg mr-1" />
+      return <a href="https://fr.reactjs.org/" target="_blank"><DiReact className="text-blue-400 text-lg mr-1" /></a>
     case "angular":
-      return <DiAngularSimple className="text-red-400 text-lg mr-1" />
+      return <a href="https://angular.io/" target="_blank"><DiAngularSimple className="text-red-400 text-lg mr-1" /></a>
     case "next":
-      return <SiNextdotjs className="text-blue-400 text-lg mr-1" />
+      return <a href="https://nextjs.org/" target="_blank"><SiNextdotjs className="text-blue-400 text-lg mr-1" /></a>
     case "mongodb" :
-      return <DiMongodb className="text-green-400 text-lg mr-1" />
+      return <a href="https://www.mongodb.com/" target="_blank"><DiMongodb className="text-green-400 text-lg mr-1" /></a>
     case "node" :
-      return <DiNodejsSmall className="text-green-400 text-lg mr-1" />
+      return <a href="https://nodejs.org/" target="_blank"><DiNodejsSmall className="text-green-400 text-lg mr-1" /></a>
     case "sass" :
-      return <DiSass className="text-pink-400 text-xl mr-1" />
+      return <a href="#" target="_blank"><DiSass className="text-pink-400 text-xl mr-1" /></a>
     case "redux" :
-      return <SiRedux className="text-purple-400 text-md mr-1" />
+      return <a href="https://redux.js.org/" target="_blank"><SiRedux className="text-purple-400 text-md mr-1" /></a>
     case "php" :
-      return <DiPhp className="text-purple-400 text-xl mr-1" />
+      return <a href="https://www.php.net/" target="_blank"><DiPhp className="text-purple-400 text-xl mr-1" /></a>
     case "firebase" :
-      return <SiFirebase className="text-yellow-400 text-lg mr-1" />
+      return <a href="https://firebase.google.com/" target="_blank"><SiFirebase className="text-yellow-400 text-lg mr-1" /></a>
     case "mysql" :
-      return <DiMysql className="text-blue-400 text-lg mr-1" />
+      return <a href="https://www.mysql.com/" target="_blank"><DiMysql className="text-blue-400 text-lg mr-1" /></a>
     case "material-ui" :
-      return <SiMaterialui className="text-blue-500 text-lg mr-1" />
+      return <a href="https://mui.com/" target="_blank"><SiMaterialui className="text-blue-500 text-lg mr-1" /></a>
     case "python" :
-      return <DiPython className="text-yellow-400 mr-1" />
+      return <a href="https://www.python.org/" target="_blank"><DiPython className="text-yellow-400 mr-1" /></a>
     case "bootstrap" :
-      return <BsFillBootstrapFill className="text-purple-700 mr-1" />
+      return <a href="https://getbootstrap.com/" target="_blank"><BsFillBootstrapFill className="text-purple-700 mr-1" /></a>
     case "tailwindcss" :
-      return <SiTailwindcss className="text-blue-700 mr-1" />
+      return <a href="https://tailwindcss.com/" target="_blank"><SiTailwindcss className="text-blue-700 mr-1" /></a>
     case "cypress" :
-        return <SiCypress className="text-purple-700 mr-1" />
+      return <a href="https://www.cypress.io/" target="_blank"><SiCypress className="text-purple-700 mr-1" /></a>
     case "google-earth" :
-        return <SiGoogleearth className="text-blue-700 mr-1" />
+      return <a href="https://earth.google.com/web/" target="_blank"><SiGoogleearth className="text-blue-700 mr-1" /></a>
     case "google-maps" :
-        return <SiGooglemaps className="text-green-700 mr-1" />
+      return <a href="https://developers.google.com/maps" target="_blank"><SiGooglemaps className="text-green-700 mr-1" /></a>
     case "express" :
-      return <SiExpress className="text-cyan-700 mr-1" />
+      return <a href="https://expressjs.com/" target="_blank"><SiExpress className="text-cyan-700 mr-1" /></a>
     case "vue" :
-      return <SiVuedotjs className="text-green-700 mr-1" />
+      return <a href="https://vuejs.org/" target="_blank"><SiVuedotjs className="text-green-700 mr-1" /></a>
     case "nuxt" :
-      return <SiNuxtdotjs className="text-green-700 mr-1" />
+      return <a href="https://nuxtjs.org/fr/" target="_blank"><SiNuxtdotjs className="text-green-700 mr-1" /></a>
     case "gcp" :
-      return <DiGoogleCloudPlatform className="text-red-700 mr-1" />
+      return <a href="https://console.cloud.google.com/" target="_blank"><DiGoogleCloudPlatform className="text-red-700 mr-1" /></a>
     case "heroku" :
-      return <DiHeroku className="text-purple-700 mr-1" />
+      return <a href="https://heroku.com/" target="_blank"><DiHeroku className="text-purple-700 mr-1" /></a>
     case "aws" :
-      return <SiAmazonaws className="text-yellow-700 mr-1" />
+      return <a href="https://aws.amazon.com/" target="_blank"><SiAmazonaws className="text-yellow-700 mr-1" /></a>
     case "redis" :
-      return <SiRedis className="text-red-700 mr-1" />
+      return <a href="https://redis.io/" target="_blank"><SiRedis className="text-red-700 mr-1" /></a>
     case "psql" :
-      return <SiPostgresql className="text-purple-700 mr-1" />
+      return <a href="https://www.postgresql.org/" target="_blank"><SiPostgresql className="text-purple-700 mr-1" /></a>
     case "graphql" :
-      return <SiGraphql className="text-pink-700 mr-1" />
+      return <a href="https://graphql.org/" target="_blank"><SiGraphql className="text-pink-700 mr-1" /></a>
     case "strapi" :
-      return <SiStrapi className="text-blue-700 mr-1" />
+      return <a href="https://strapi.io/" target="_blank"><SiStrapi className="text-blue-700 mr-1" /></a>
     case "typescript" :
-      return <SiTypescript className="text-blue-700 mr-1" />
+      return <a href="https://www.typescriptlang.org/" target="_blank"><SiTypescript className="text-blue-700 mr-1" /></a>
     case "html" :
-      return <SiHtml5 className="text-red-700 mr-1" />
+      return <a href="https://www.w3.org/html/" target="_blank"><SiHtml5 className="text-red-700 mr-1" /></a>
     case "css" :
-      return <SiCss3 className="text-blue-700 mr-1" />
+      return <a href="https://www.w3schools.com/css/" target="_blank"><SiCss3 className="text-blue-700 mr-1" /></a>
     case "selligent" :
-      return <img className="w-6 mr-1" src="https://i.imgur.com/x0NSR4G.png"/>
+      return <a href="https://www.selligent.com/" target="_blank"><img className="w-6 mr-1" src="https://i.imgur.com/x0NSR4G.png"/></a>
     default:
       return <></>
   }
